610804e57def2cccdfe118773909ac5b2723b9bc
andresLnx <andreserluis@gmail.com>
2023-02-16 17:56:14 -0500

    Provider:  middlewares added to the route /api/provider and controllers ready to conect with  the Provider model

--- src/controllers/provider.js
+++ src/controllers/provider.js
@@ -1,26 +1,88 @@
-const getProviders = async () => {
+const { request, response } = require("express");
+// const Provider = require('../models/Provider');
+
+const getProviders = async (req = request, resp = response) => {
   try {
-  } catch (error) {}
+    // const providers = await Provider.find();
+    return resp.json({
+      ok: true,
+      data: "Uncomment the line above and put the providers here when the model is ready",
+    });
+  } catch (error) {
+    console.log(error);
+    resp.status(500).json({
+      ok: false,
+      msg: "Contacte al admin",
+      error,
+    });
+  }
 };
 
-const getProviderById = async () => {
+const getProviderById = async (req = request, resp = response) => {
   try {
-  } catch (error) {}
+    const { id } = req.params;
+    // const provider = (await Provider.findById()) || false;
+
+    // Check if there is not a provider
+    // if (!provider) {
+    //   return resp.json({
+    //     ok: false,
+    //     data: null,
+    //     msg: `We could not find an element with this id(${id})`,
+    //   });
+    // }
+
+    return resp.json({
+      ok: true,
+      data: {},
+    });
+  } catch (error) {
+    console.log(error);
+    resp.status(500).json({
+      ok: false,
+      msg: "Contacte al admin",
+      error,
+    });
+  }
 };
 
-const insertProvider = async () => {
+const insertProvider = async (req = request, resp = response) => {
   try {
-  } catch (error) {}
+    return resp.json({ ok: true, msg: "Al validations passed" });
+  } catch (error) {
+    console.log(error);
+    resp.status(500).json({
+      ok: false,
+      msg: "Contacte al admin",
+      error,
+    });
+  }
 };
 
-const updateProvider = async () => {
+const updateProvider = async (req = request, resp = response) => {
   try {
-  } catch (error) {}
+    return resp.json({ ok: true, msg: "Al validations passed" });
+  } catch (error) {
+    console.log(error);
+    resp.status(500).json({
+      ok: false,
+      msg: "Contacte al admin",
+      error,
+    });
+  }
 };
 
 const deleteProvider = async () => {
   try {
-  } catch (error) {}
+    return resp.json({ ok: true, msg: "Al validations passed" });
+  } catch (error) {
+    console.log(error);
+    resp.status(500).json({
+      ok: false,
+      msg: "Contacte al admin",
+      error,
+    });
+  }
 };
 
 module.exports = {
--- src/routes/provider.js
+++ src/routes/provider.js
@@ -15,12 +15,37 @@ const { Router } = require("express");
 
 // Router object
 const router = new Router();
-
+const { checkForErrors } = require("../middlewares/checkForErrors");
 // Routes
 router.get("/", [], getProviders);
-router.get("/:id", [], getProviderById);
-router.post("/new", [], insertProvider);
-router.put("/update/:id", [], updateProvider);
-router.delete("/delete/:id", [], deleteProvider);
+router.get(
+  "/:id",
+  [param("id", "Not a valid mongo id").isMongoId(), checkForErrors],
+  getProviderById
+);
+router.post(
+  "/new",
+  [
+    body("name", "The name is required").notEmpty(),
+    body("name", "The name should be a String").isString(),
+    checkForErrors,
+  ],
+  insertProvider
+);
+router.put(
+  "/update/:id",
+  [
+    param("id", "Not a valid mongo id").isMongoId(),
+    body("name", "The name is required").notEmpty(),
+    body("name", "The name should be a String").isString(),
+    checkForErrors,
+  ],
+  updateProvider
+);
+router.delete(
+  "/delete/:id",
+  [param("id", `Invalid value `).isMongoId(), checkForErrors],
+  deleteProvider
+);
 
 module.exports = router;
